72ca8d62716b32faf563fc4bc56389e8d4bc1d6a
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:21:50 +0300

    my first app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello Kevin!");
+            /*3.The program greets the user by their name*/
+            /*2.The user enters their name*/
+            /*"1.ask the user`s name "*/
+            Console.WriteLine("What`s your name?");
+            string Name;
+            //read the input  from the user
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+        }
+    }
+}
